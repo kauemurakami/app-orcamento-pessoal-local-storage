376e3b374565711f2e60fec373d2e11be2fba6ba
kauê murakami <kauetmurakami@gmail.com>
2019-05-27 12:46:29 -0300

    classe despesa

--- app.js
+++ app.js
@@ -1,5 +1,35 @@
+class Despesa{
+	constructor(ano, mes, dia, tipo, descricao, valor){
+		this.ano = ano
+		this.mes = mes
+		this.dia = dia
+		this.tipo = tipo
+		this.descricao = descricao
+		this.valor = valor
+	}
+}
 
+function cadastrarDespesa(){
+	//let ano = document.getElementById('ano').value
+	//console.log(ano)
+	//mas as vezes devemos selecionar todo o elemento nao só o value
+	let ano = document.getElementById('ano')
+	let mes = document.getElementById('mes')
+	let dia = document.getElementById('dia')
+	let tipo = document.getElementById('tipo')
+	let descricao = document.getElementById('descricao')
+	let valor = document.getElementById('valor')
 
-cadastrarDespesa(){
-alert('ois')
+	/*console.log(ano.value)
+	console.log(mes.value)
+	console.log(dia.value)
+	console.log(tipo.value)
+	console.log(descricao.value)
+	console.log(valor.value)*/
+
+	let despesa = new Despesa(
+			ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value
+		)
+
+	console.log(despesa)
 }
